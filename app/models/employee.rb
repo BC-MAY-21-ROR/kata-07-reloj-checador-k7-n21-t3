60f756fc11fbbb8daa73ac35a5e6db1b9675b6c7
DerekAyala <derek.ayala12345@hotmail.com>
2022-01-27 20:52:53 -0600

    Validate unique email,employee number , private number
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,5 @@ class Employee < ApplicationRecord
   belongs_to :company
   has_many :attendance # , dependent: :destroy
   validate :name, :email, :position, :employee_num, :private_num, :company_id, presence: true
+  validate :email, :employee_num, :private_num, uniqueness: true
 end
