0e3918dafd04a2038251149a26f91471adf23496
DerekAyala <derek.ayala12345@hotmail.com>
2022-01-27 20:16:00 -0600

    validate presence in the models
    - Employee
    - Company
    - Attendance
    
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,4 @@
 class Attendance < ApplicationRecord
   belongs_to :employee
+  validate :employee_id, :attendance_date, presence: true
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
   belongs_to :company
   has_many :attendance # , dependent: :destroy
+  validate :name, :email, :position, :employee_num, :private_num, :company_id, presence: true
 end
