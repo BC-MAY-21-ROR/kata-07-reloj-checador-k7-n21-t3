906f2e26b12a5907ebd8ff4d74ffc4189c49c551
DerekAyala <derek.ayala12345@hotmail.com>
2022-01-27 21:27:10 -0600

    Routes errors fixed
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'login/admin'
   root 'pages#home'
   get 'admin', to: 'dashboard_admin#dashboard_admin'
-  get "/employees", to: "employee#index"
+  get "/employees", to: "employees#index"
   get "absence", to: 'absence#absence'
   get "/companies", to: "companies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
