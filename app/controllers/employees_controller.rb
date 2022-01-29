bc7e2b209beb3a449c363bda6c5d51cdd79d701c
DerekAyala <derek.ayala12345@hotmail.com>
2022-01-28 19:28:44 -0600

    employee controller methods ready - index - new - create - edit - update - destroy
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,37 @@
 class EmployeesController < ApplicationController
   def index
+    @employee = Employee.where(disable: false)
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(params.require(:employee).permit(:name, :email, :position, :employee_num, :private_num, :company_id))
+    if @employee.save
+      redirect_to companies_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(params.require(:employee).permit(:name, :email, :position, :employee_num, :private_num, :company_id))
+      redirect_to companies_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.update(disable: true)
+    redirect_to companies_path
   end
 end
