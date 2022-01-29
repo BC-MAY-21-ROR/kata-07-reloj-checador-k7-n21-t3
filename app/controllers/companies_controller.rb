edc98ff11718948ef4fe1ae5bbaea399eb7bf2ea
DerekAyala <derek.ayala12345@hotmail.com>
2022-01-28 18:05:04 -0600

    Create method with validations
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,8 +3,15 @@ class CompaniesController < ApplicationController
     @company = Company.all
   end
   def new
+    @company = Company.new
   end
 
   def create
+    @company = Company.new(params.require(:company).permit(:name, :address))
+    if @company.save
+      redirect_to companies_path
+    else
+      render 'new'
+    end
   end
 end
