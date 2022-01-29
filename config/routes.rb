89a55bfb64ccdb8763a7a03eabb212114dd485da
DerekAyala <derek.ayala12345@hotmail.com>
2022-01-28 19:18:30 -0600

    select the resources for employees - edit - update - index - destroy - new - create
    Co-authored-by: Anisbeflo <abejarano0@ucol.mx>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'login/admin'
   root 'pages#home'
   get 'admin', to: 'dashboard_admin#dashboard_admin'
-  get "/employees", to: "employees#index"
+  resources :employees, only: [:edit, :update, :index, :destroy, :new, :create]
   get "absence", to: 'absence#absence'
   resources :companies, only: [:edit, :update, :index, :destroy, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
