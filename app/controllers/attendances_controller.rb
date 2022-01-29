0e995efbaaee4432dbebab8f27987982511bd385
Anisbeflo <abejarano0@ucol.mx>
2022-01-28 19:59:11 -0600

    Advances method create in attendances_controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,14 @@
 class AttendancesController < ApplicationController
   def index
-    @attendance = attendance.new
+    @attendance = Attendance.new
   end
 
   def create
+    emp = Employee.find_by private_num: :private_num
+    time = Time.now.strftime("%H:%M")
+    day = Time.now.strftime("%d-%m-%Y")
+
+    @attendance = Attendance.create(employee_id: emp.id, check_in: time, attendance_date: day)
+    render json: @attendance
   end
 end
