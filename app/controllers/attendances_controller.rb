17b34453aaa7539baaa694eea365b0f3d6fc60b3
Anisbeflo <abejarano0@ucol.mx>
2022-01-29 20:57:20 -0600

    Progress in check in button
    - home.html.erb is now in the attendances controller, and it's name is
      new.html.erb now
    - attendances_controller.rb now has the method create
    - new.html.erb is now root
    - routes have all the method from the attendance controller
    
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,14 +1,48 @@
 class AttendancesController < ApplicationController
   def index
+  end
+
+  def new
     @attendance = Attendance.new
+    @company = Company.where(disable: false)
   end
 
   def create
-    emp = Employee.find_by private_num: :private_num
-    time = Time.now.strftime("%H:%M")
-    day = Time.now.strftime("%d-%m-%Y")
-
-    @attendance = Attendance.create(employee_id: emp.id, check_in: time, attendance_date: day)
-    render json: @attendance
+    @employee = Employee.where(disable:false)
+    data = params.require(:attendance).permit(:private_num,:company_id)
+    time = Time.now.strftime("%H:%M:%S")
+    day = Time.now.strftime("%m-%d-%Y")
+    emp = @employee.find_by(private_num: data.fetch(:private_num))
+    com = @employee.find_by(company_id: data.fetch(:company_id))
+    if emp.nil? and com.nil?
+      @all = Attendance.all
+      update = @all.find_by(private_num: data.fetch(:private_num), attendance_date: day)
+      if update.nil?
+        if Attendance.new(attendance_date:day, check_in:time, private_num: data.fetch(:private_num)).save
+          redirect_to new_attendances_path
+        else
+          render 'new'
+        end
+      else
+        update.update(check_out: time)
+      end
+    end
   end
 end
+
+# @employee = Employee.where(disable:false)
+#     data = params.require(:attendance).permit(:private_num,:company_id)
+#     private_num = data.fetch(:private_num)
+#     company_id = data.fetch(:company_id)
+#     time = "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')}"
+#     day = "#{Time.now.strftime('%Y')}-#{Time.now.strftime('%m')}-#{Time.now.strftime('%e')}"
+#     verify_check_in = Attendance.find_by(check_out:nil, private_num: private_num)
+#     if verify_check_in.nil?
+#       if Attendance.create(attendance_date: day, check_in: time, check_out: nil, private_num: private_num).save
+#         redirect_to new_attendances_path
+#       else
+#         render 'new'
+#       end
+#     else
+#       make_check_out = Attendance.find_by(check_out: nil, private_num: private_num)
+#       make_check_out = 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @attendance = Attendance.new
+  end
+  def create
+    time = Time.now.strftime("%H:%M")
+    day = Time.now.strftime("%d-%m-%Y")
+
+    @attendance = Attendance.create(attendance_date: day, check_in: time, check_out: time, private_num: :private_num)
+    if @attendance.save
+      redirect_to pages_path
+    end
   end
 end
