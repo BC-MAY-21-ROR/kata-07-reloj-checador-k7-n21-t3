0f085c901c3c9d9225b5e0dfded90256c02fe1ef
Anisbeflo <abejarano0@ucol.mx>
2022-02-01 16:40:27 -0600

    Added styles to the alert
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: MauricioNC <mauricio_navarro0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -38,20 +38,3 @@ class AttendancesController < ApplicationController
     end
   end
 end
-
-# @employee = Employee.where(disable:false)
-#     data = params.require(:attendance).permit(:private_num,:company_id)
-#     private_num = data.fetch(:private_num)
-#     company_id = data.fetch(:company_id)
-#     time = "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')}"
-#     day = "#{Time.now.strftime('%Y')}-#{Time.now.strftime('%m')}-#{Time.now.strftime('%e')}"
-#     verify_check_in = Attendance.find_by(check_out:nil, private_num: private_num)
-#     if verify_check_in.nil?
-#       if Attendance.create(attendance_date: day, check_in: time, check_out: nil, private_num: private_num).save
-#         redirect_to new_attendances_path
-#       else
-#         render 'new'
-#       end
-#     else
-#       make_check_out = Attendance.find_by(check_out: nil, private_num: private_num)
-#       make_check_out =
