45684251250568d2ff6727021964122dd847213e
Anisbeflo <abejarano0@ucol.mx>
2022-01-31 18:48:07 -0600

    Added alerts to give feedback to the user about a successfully check in/out

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -12,10 +12,9 @@ class AttendancesController < ApplicationController
     data = params.require(:attendance).permit(:private_num,:company_id)
     time = Time.now.strftime("%H:%M:%S")
     day = Time.now.strftime("%d-%m-%Y")
-    emp = @employee.find_by(private_num: data.fetch(:private_num))
-    com = @employee.find_by(company_id: data.fetch(:company_id))
+    emp = @employee.find_by(private_num: data.fetch(:private_num), company_id: data.fetch(:company_id))
 
-    if !emp.nil? and !com.nil?
+    if !emp.nil?
       @all = Attendance.all
       update = @all.find_by(private_num: data.fetch(:private_num), attendance_date: day, check_out: nil)
       if update.nil?
@@ -23,19 +22,19 @@ class AttendancesController < ApplicationController
         data[:check_in]=time
         data.delete(:company_id)
         if Attendance.new(data).save
-          redirect_to root_path, info: "Invalid email"
+          redirect_to root_path, success: "Check In Successfully"
         else
           redirect_to root_path
         end
       else
         if update.update(check_out: time)
-          redirect_to root_path
+          redirect_to root_path, success: "Check Out Successfully"
         else
           redirect_to root_path
         end
       end
     else
-      redirect_to root_path
+      redirect_to root_path, danger: "Private Number or Company are wrong"
     end
   end
 end
