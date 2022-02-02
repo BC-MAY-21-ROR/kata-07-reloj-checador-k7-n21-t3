9aaa51646ca373b2ab27b4a4564e5f0cb20bbdb1
Anisbeflo <abejarano0@ucol.mx>
2022-02-02 11:48:50 -0600

    Functionality of Attendance Index
    - Find all the record from the table attendance that match the chosen
      date
    - Show in table: Private number, employee name, company name, date, time
      check in and out
    
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,11 @@
 class AttendancesController < ApplicationController
   def index
+    date = params[:date]
+    if !date.nil?
+      @attendance = Attendance.where(attendance_date: date)
+    else
+      @attendance = Attendance.where(attendance_date: Time.now)
+    end
+    render 'index'
   end
 end
